6cf96b175b2cb3b299bc0e5447ede0ef0d57209d
Jolanta Galczynska <jolanta.galczynska@gmail.com>
2021-04-15 19:53:56 +0200

    style changes

--- src/components/pages/TimeTable.js
+++ src/components/pages/TimeTable.js
@@ -9,13 +9,13 @@ function TimeTable() {
       <div className="timetable">
           <h1 className="welcome">Adult/Mature Age Beginner Dance Classes Perth</h1>
             <img className="pic" src="images/dance.png" alt="TimeTable" width="90%" />
-              <p className="info_schedule">All of our classes allow for drop-in attendance</p>
+              <p>All of our classes allow for drop-in attendance</p>
               <p>All classes are 75min</p>
               <p>Please contact us for more information</p>
-      </div>
-    <div className="copy-rights">
-        <p>DanseSkole<i className="far fa-copyright"></i>2021</p>
-    </div>
+              <div className="copy-rights">
+              <p className="copy-right__footer">DanseSkole<i className="far fa-copyright"></i>2021</p>
+            </div>
+        </div>
     </>
   );
 }
