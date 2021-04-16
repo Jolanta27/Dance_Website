fe517eaf7c00849b02237993d5c051d0820cf9fe
Jolanta Galczynska <jolanta.galczynska@gmail.com>
2021-04-16 15:09:49 +0200

    changes in mobile version

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './styles/Newsletter.css';
 import MailchimpSubscribe from "react-mailchimp-subscribe";
 class Newsletter extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+        constructor() {
+            super();
+            this.state = {
             email: ''
         };
     }
+
     mySubmitHandler = (event) => {
         event.preventDefault(); 
         event.target.reset();
@@ -15,9 +16,9 @@ class Newsletter extends React.Component {
     render() {
         return (
             <form className="newsletter_form" onSubmit={this.mySubmitHandler.bind(this)}>
-                <h1 style={{fontSize: 40, paddingTop: '10px'}}>Newsletter</h1>
+                <h1>Newsletter</h1>
                 <p className="newsletter_info">Stay up to date with dance news in Oslo with us</p>
-                <MailchimpSubscribe value="subscribe" url={process.env.REACT_APP_MAILCHIMP_URL} />
+                <MailchimpSubscribe value="subscribe" url={process.env.REACT_APP_MAILCHIMP_URL}/>
                 <p className="newsletter_info">Your email is safe with us.</p> 
                 <p className="newsletter_info"> We don't spam.</p>
             </form>
